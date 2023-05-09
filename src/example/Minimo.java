3569c56181aa3baa074b35d3bca088fde0b9f0ea
Claudia Mosca <claudiam9700@gmail.com>
2023-05-09 10:35:09 +0200

    min exercise

---
+++ src/example/Minimo.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Minimo {
+    public static int min(int[] numbers) {
+        int result = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (result > numbers[i]) {
+                result = numbers[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 3, 9, 1 };
+        int result = min(values);
+        System.out.println(result);
+    }
+}
